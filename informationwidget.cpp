e1f9fc627f6c829ad21584f5b10c5e13e7ec4828
basarudin <ahmad.basarudin@gmail.com>
2019-07-24 15:37:00 +0700

    resett

--- informationwidget.cpp
+++ informationwidget.cpp
@@ -6,8 +6,6 @@
 InformationWidget::InformationWidget(QWidget *parent)
     : QWidget(parent)
     , m_infoLabel(new QLabel)
-    // 使用 "/home" 初始化 QStorageInfo
-    // 如果 "/home" 没有挂载到一个单独的分区上，QStorageInfo 收集的数据将会是根分区的
 {
     m_infoLabel->setStyleSheet("QLabel {"
                                "color: white;"
@@ -26,7 +24,6 @@ InformationWidget::InformationWidget(QWidget *parent)
 
 void InformationWidget::UpdateData(const QString &cpu,const QString &mem,const QString &sbytes,const QString &rbytes)
 {
-<<<<<<< HEAD
 
     QString s_cpu = cpu;
     s_cpu.replace(QString("%"), QString(""));
@@ -62,9 +59,3 @@ void InformationWidget::UpdateData(const QString &sbytes,const QString &rbytes)
                          .arg(sbytes)
                          .arg(rbytes));
 }
-=======
-	m_infoLabel->setText(QString("CPU:%1↑%2/S\nMEM:%3↓%4/S")
-						 .arg(cpu).arg(sbytes)
-						 .arg(mem).arg(rbytes));
-}
->>>>>>> parent of 83b134b... repair fashion mode display bug
